81a4667f1c68c1d409d3f39c63255a90d696565c
Robert Serban <serbanrobert43@yahoo.com>
2025-02-28 15:34:57 +0200

    another style

--- react-site/site/src/components/ServicesSection.jsx
+++ react-site/site/src/components/ServicesSection.jsx
@@ -4,69 +4,108 @@ import diaphragm from "../img/diaphragm.svg";
 import money from "../img/money.svg";
 import teamwork from "../img/teamwork.svg";
 import home2 from "../img/home2.png";
+import styled from "styled-components";
+import { About, Description, Image } from "../styles";
 
 const ServicesSection = () => {
   return (
-    <div className="services">
-      <div className="description">
+    <Services>
+      <Description>
         <h2>
           High <span>quality</span> services
         </h2>
-        <div className="cards">
-          <div className="card">
+        <Cards>
+          <Card>
             <div className="icon">
               <img src={clock} alt="icon" />
               <h3>Efficient</h3>
             </div>
-            <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis
-              facilis molestiae natus, animi illo iusto nemo quod et dolorem
-              totam inventore dicta enim tempore assumenda porro repudiandae
-              velit repellat fugiat.
-            </p>
-          </div>
-          <div className="card">
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+          </Card>
+          <Card>
             <div className="icon">
               <img src={teamwork} alt="icon" />
               <h3>Teamwork</h3>
             </div>
-            <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis
-              facilis molestiae natus, animi illo iusto nemo quod et dolorem
-              totam inventore dicta enim tempore assumenda porro repudiandae
-              velit repellat fugiat.
-            </p>
-          </div>
-          <div className="card">
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+          </Card>
+          <Card>
             <div className="icon">
               <img src={diaphragm} alt="icon" />
               <h3>Diaphragm</h3>
             </div>
-            <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis
-              facilis molestiae natus, animi illo iusto nemo quod et dolorem
-              totam inventore dicta enim tempore assumenda porro repudiandae
-              velit repellat fugiat.
-            </p>
-          </div>
-          <div className="card">
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+          </Card>
+          <Card>
             <div className="icon">
               <img src={money} alt="icon" />
               <h3>Affordable</h3>
             </div>
-            <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis
-              facilis molestiae natus, animi illo iusto nemo quod et dolorem
-              totam inventore dicta enim tempore assumenda porro repudiandae
-              velit repellat fugiat.
-            </p>
-          </div>
-        </div>
-      </div>
-      <div className="image">
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+          </Card>
+        </Cards>
+      </Description>
+      <Image>
         <img src={home2} alt="icon" />
-      </div>
-    </div>
+      </Image>
+    </Services>
   );
 };
+
+const Services = styled(About)`
+  min-height: 90vh;
+  color: white;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 5rem 10rem;
+  h2 {
+    padding-bottom: 5rem;
+  }
+  p {
+    width: 70%;
+    padding: 2rem 0rem 4rem 0rem;
+  }
+  @media (max-width: 1500px) {
+    display: block;
+    padding: 2rem 2rem;
+  }
+`;
+
+const Cards = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 2rem; /* Adaugă spațiu între elemente */
+
+  @media (max-width: 1500px) {
+    justify-content: space-between;
+  }
+`;
+
+const Card = styled.div`
+  flex: 1 1 calc(50% - 2rem); /* 50% din container, cu spațiu între ele */
+  max-width: 400px; /* Evită extinderea pe ecrane mari */
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+
+  .icon {
+    display: flex;
+    align-items: center;
+
+    h3 {
+      margin-left: 1rem;
+      background: white;
+      color: black;
+      padding: 1rem;
+    }
+  }
+
+  @media (max-width: 768px) {
+    flex: 1 1 100%; /* Pe ecrane mici, 1 pe rând */
+  }
+`;
+
 export default ServicesSection;
