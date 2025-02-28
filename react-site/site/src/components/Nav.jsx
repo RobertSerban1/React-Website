57e424a76f76a93d184d042fdb128f13719d5660
Robert Serban <serbanrobert43@yahoo.com>
2025-02-28 16:37:44 +0200

    more about pages + react router

---
+++ react-site/site/src/components/Nav.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+const Nav = () => {
+  return (
+    <StyledNav>
+      <h1>
+        <Link id="Logo" to="/">
+          Capture
+        </Link>
+      </h1>
+      <ul>
+        <li>
+          <Link to="/">About us</Link>
+        </li>
+        <li>
+          <Link to="/work">Our work</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact us</Link>
+        </li>
+      </ul>
+    </StyledNav>
+  );
+};
+
+const StyledNav = styled.nav`
+  min-height: 10vh;
+  display: flex;
+  margin: auto;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem 10rem;
+  background: #282828;
+  a {
+    color: white;
+    text-decoration: none;
+  }
+  ul {
+    display: flex;
+    list-style: none;
+  }
+  #Logo {
+    font-size: 1.5rem;
+    font-family: "Lobster", cursive;
+    font-weight: lighter;
+  }
+  li {
+    padding-left: 10rem;
+    position: relative;
+  }
+`;
+
+export default Nav;
