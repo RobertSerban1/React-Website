f8c044dd54b95420cc162a613131a8f1c6892f67
Robert Serban <serbanrobert43@yahoo.com>
2025-03-03 23:14:23 +0200

    animation for pages

--- react-site/site/src/App.jsx
+++ react-site/site/src/App.jsx
@@ -4,20 +4,25 @@ import GlobalStyle from "./components/GlobalStyle";
 import Nav from "./components/Nav";
 import ContactUs from "./pages/ContactUs";
 import OurWork from "./pages/OurWork";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import MovieDetail from "./pages/MovieDetail";
+import { AnimatePresence } from "framer-motion";
 
 function App() {
+  const location = useLocation();
+
   return (
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <Routes>
-        <Route path="/" element={<AboutUs />} />
-        <Route path="/work" element={<OurWork />} />
-        <Route path="/work/:id" element={<MovieDetail />} />
-        <Route path="/contact" element={<ContactUs />} />
-      </Routes>
+      <AnimatePresence mode="wait">
+        <Routes location={location} key={location.pathname}>
+          <Route path="/" element={<AboutUs />} />
+          <Route path="/work" element={<OurWork />} />
+          <Route path="/work/:id" element={<MovieDetail />} />
+          <Route path="/contact" element={<ContactUs />} />
+        </Routes>
+      </AnimatePresence>
     </div>
   );
 }
--- react-site/site/src/components/AboutSection.jsx
+++ react-site/site/src/components/AboutSection.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import home1 from "../img/home1.png";
+import { motion } from "framer-motion";
 import styled from "styled-components";
 import { About, Description, Image, Hide } from "../styles";
 
@@ -7,19 +8,21 @@ const AboutSection = () => {
   return (
     <About>
       <Description>
-        <div className="title">
+        <motion.div>
           <Hide>
-            <h2>We work to make</h2>
+            <motion.h2>We work to make</motion.h2>
           </Hide>
           <Hide>
-            <h2>
+            <motion.h2>
               your <span>dreams</span> come{" "}
-            </h2>
+            </motion.h2>
           </Hide>
           <Hide>
-            <h2>true</h2>
+            <motion.h2>
+              <h2>true</h2>
+            </motion.h2>
           </Hide>
-        </div>
+        </motion.div>
         <p>
           Contact us for any photography or videography ideas that you have.
         </p>
