900f2c3d7706c96d8a442ec3d1089aaf0e80890e
Robert Serban <serbanrobert43@yahoo.com>
2025-03-11 00:04:09 +0200

    finish and animate faq section

--- react-site/site/src/components/Faq.jsx
+++ react-site/site/src/components/Faq.jsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { About } from "../styles.jsx";
+import Toggle from "./Toggle.jsx";
+import { LayoutGroup } from "framer-motion";
 
 const FaqSection = () => {
   return (
@@ -8,62 +10,56 @@ const FaqSection = () => {
       <h2>
         Any Questions <span>FAQ</span>
       </h2>
-      <div className="question">
-        <h4>How do I start?</h4>
-        <div className="answer">
-          <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic
-            tempora.
-          </p>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus,
-            similique?
-          </p>
-        </div>
-        <div className="faq-line"></div>
-      </div>
-      <div className="question">
-        <h4>Daily Schedule</h4>
-        <div className="answer">
-          <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic
-            tempora.
-          </p>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus,
-            similique?
-          </p>
-        </div>
-        <div className="faq-line"></div>
-      </div>
-      <div className="question">
-        <h4>Different Payment Methods</h4>
-        <div className="answer">
-          <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic
-            tempora.
-          </p>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus,
-            similique?
-          </p>
-        </div>
-        <div className="faq-line"></div>
-      </div>
-      <div className="question">
-        <h4>What Products do you offer</h4>
-        <div className="answer">
-          <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic
-            tempora.
-          </p>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus,
-            similique?
-          </p>
-        </div>
-        <div className="faq-line"></div>
-      </div>
+      <LayoutGroup>
+        <Toggle title="How do I start?">
+          <div className="answer">
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic
+              tempora.
+            </p>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Doloribus, similique?
+            </p>
+          </div>
+        </Toggle>
+        <Toggle title="Daily Schedule">
+          <div className="answer">
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic
+              tempora.
+            </p>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Doloribus, similique?
+            </p>
+          </div>
+        </Toggle>
+        <Toggle title="Different Payment Methods">
+          <div className="answer">
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic
+              tempora.
+            </p>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Doloribus, similique?
+            </p>
+          </div>
+        </Toggle>
+        <Toggle title="What Products do you offer">
+          <div className="answer">
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic
+              tempora.
+            </p>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Doloribus, similique?
+            </p>
+          </div>
+        </Toggle>
+      </LayoutGroup>
     </Faq>
   );
 };
