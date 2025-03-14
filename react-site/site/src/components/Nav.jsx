315bb3d0b5e478b755de5b2dd804d534a5bd6789
Robert Serban <serbanrobert43@yahoo.com>
2025-03-14 18:00:20 +0200

    mobile friendly

--- react-site/site/src/components/Nav.jsx
+++ react-site/site/src/components/Nav.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import styled from "styled-components";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import { motion } from "framer-motion";
 
 const Nav = () => {
+  const { pathname } = useLocation();
+
   return (
     <StyledNav>
       <h1>
@@ -13,18 +16,34 @@ const Nav = () => {
       <ul>
         <li>
           <Link to="/">About us</Link>
+          <Line
+            transition={{ duration: 0.75 }}
+            initial={{ width: "0%" }}
+            animate={{ width: pathname === "/" ? "50%" : "0%" }}
+          />
         </li>
         <li>
           <Link to="/work">Our work</Link>
+          <Line
+            transition={{ duration: 0.75 }}
+            initial={{ width: "0%" }}
+            animate={{ width: pathname === "/work" ? "50%" : "0%" }}
+          />
         </li>
         <li>
           <Link to="/contact">Contact us</Link>
+          <Line
+            transition={{ duration: 0.75 }}
+            initial={{ width: "0%" }}
+            animate={{ width: pathname === "/contact" ? "50%" : "0%" }}
+          />
         </li>
       </ul>
     </StyledNav>
   );
 };
 
+// Styled Components
 const StyledNav = styled.nav`
   min-height: 10vh;
   display: flex;
@@ -58,7 +77,6 @@ const StyledNav = styled.nav`
     padding: 2rem 1rem;
     #Logo {
       display: inline-block;
-      margin: ;
     }
     ul {
       padding: 2rem;
@@ -68,6 +86,18 @@ const StyledNav = styled.nav`
         padding: 0;
       }
     }
+  }
+`;
+
+const Line = styled(motion.div)`
+  height: 0.3rem;
+  background: #23d997;
+  width: 0%;
+  position: absolute;
+  bottom: -80%;
+  left: 60%;
+  @media (max-width: 1300px) {
+    left: 0%;
   }
 `;
 
